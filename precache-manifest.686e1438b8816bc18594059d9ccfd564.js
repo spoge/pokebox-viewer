62f9f9b3003d3f3798fedb8a292446f11d0d8ce6
Sondre Sparby Boge <sondreboge@hotmail.com>
2020-09-27 22:03:35 +0200

    Updates

--- precache-manifest.686e1438b8816bc18594059d9ccfd564.js
+++ precache-manifest.686e1438b8816bc18594059d9ccfd564.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a710593156912df1f69a125371adf5ec",
+    "revision": "173d1f5c89257255fcc06fa7fce3d742",
     "url": "/pokemon-box-calculator/index.html"
   },
   {
-    "revision": "62bf8c467a798bebb36c",
-    "url": "/pokemon-box-calculator/static/css/main.c3a2c6a1.chunk.css"
+    "revision": "e6cc3d6507820bb267e7",
+    "url": "/pokemon-box-calculator/static/css/main.d101cfa3.chunk.css"
   },
   {
     "revision": "bae657042420a8b6f811",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pokemon-box-calculator/static/js/2.b8bef70f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "62bf8c467a798bebb36c",
-    "url": "/pokemon-box-calculator/static/js/main.c1b8ea42.chunk.js"
+    "revision": "e6cc3d6507820bb267e7",
+    "url": "/pokemon-box-calculator/static/js/main.cea998b3.chunk.js"
   },
   {
     "revision": "4f6e35a201e4b9066f42",
